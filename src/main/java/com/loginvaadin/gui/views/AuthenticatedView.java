f6b210175baa325a05ef126df908cfc2aea153e1
Martin Ondrejka <martin.ondrejka@gmail.com>
2023-12-13 18:16:01 +0100

    added logout feature

--- src/main/java/com/loginvaadin/gui/views/AuthenticatedView.java
+++ src/main/java/com/loginvaadin/gui/views/AuthenticatedView.java
@@ -2,6 +2,8 @@ package com.loginvaadin.gui.views;
 
 import com.loginvaadin.backend.entities.Book;
 import com.loginvaadin.backend.services.BookService;
+import com.loginvaadin.backend.services.SecurityService;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -11,11 +13,17 @@ import org.vaadin.crudui.crud.impl.GridCrud;
 @Route("books")
 @PermitAll
 public class AuthenticatedView extends VerticalLayout {
-	public AuthenticatedView(BookService service) {
+	public AuthenticatedView(BookService service, SecurityService securityService) {
+
+		var btnLogout = new Button("logout");
+		btnLogout.addClickListener(buttonClickEvent -> {
+			securityService.logout();
+		} );
+
 		var crud = new GridCrud<>(Book.class, service);
 		crud.getGrid().setColumns("title", "published", "rating");
 		crud.getCrudFormFactory().setVisibleProperties("title", "published", "rating");
 
-		add(new H1("Books in library..."), crud);
+		add(btnLogout, new H1("Books in library..."), crud);
 	}
 }
--- src/main/java/com/loginvaadin/gui/views/LoginView.java
+++ src/main/java/com/loginvaadin/gui/views/LoginView.java
@@ -1,9 +1,11 @@
 package com.loginvaadin.gui.views;
 
 import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @Route("login")
+@PageTitle("Login | Vaadin library")
 public class LoginView extends Composite<LoginOverlayFooter> {
 	public LoginView() {
 		// TODO add logout
